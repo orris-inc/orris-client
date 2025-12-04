8f84327be87206729bfe59c776266b50b62609f8
orris-inc <orrisince@gmail.com>
2025-12-04 18:27:08 +0800

    refactor: rename orris-agent to orris-client and remove Windows builds
    - Rename cmd/orris-agent to cmd/orris-client
    - Update Makefile, install script and CI workflows
    - Remove Windows from build matrix (linux/darwin only)
    - Fix gitignore to not ignore cmd/orris-client directory

---
+++ cmd/orris-client/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/easayliu/orris-client/internal/agent"
+	"github.com/easayliu/orris-client/internal/config"
+	"github.com/easayliu/orris-client/internal/logger"
+	flag "github.com/spf13/pflag"
+)
+
+func main() {
+	var (
+		serverURL = flag.StringP("server", "u", "", "server URL")
+		token     = flag.StringP("token", "t", "", "bearer token")
+	)
+	flag.Parse()
+
+	cfg := config.LoadFromEnv()
+
+	if *serverURL != "" {
+		cfg.ServerURL = *serverURL
+	}
+	if *token != "" {
+		cfg.Token = *token
+	}
+
+	if cfg.Token == "" {
+		logger.Error("token is required (use --token or ORRIS_TOKEN env)")
+		os.Exit(1)
+	}
+
+	logger.Info("starting orris-agent", "server", cfg.ServerURL)
+
+	ag := agent.New(cfg)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	if err := ag.Start(ctx); err != nil {
+		logger.Error("failed to start agent", "error", err)
+		os.Exit(1)
+	}
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+
+	<-sigChan
+	logger.Info("shutting down")
+
+	ag.Stop()
+	logger.Info("agent stopped")
+}
